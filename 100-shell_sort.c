4fca21da0ef98d62af8a084f32740998ed275606
Peter Brian <wahomebrian98@gmail.com>
2023-11-25 00:50:46 +0300

    shell sort

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,30 +1,36 @@
 #include "sort.h"
 
+/**
+ * shell_sort - sorts array of integer using Shell sort algorithm
+ * @array: the pointer to the array
+ * @size: the size of the array
+ * Return: void
+ */
 void shell_sort(int *array, size_t size)
 {
-    size_t interval = 1, i, j;
-    int temp;
+size_t interval = 1, i, j;
+int temp;
 
-    if (array == NULL || size < 2)
-        return;
+if (array == NULL || size < 2)
+return;
 
-    while (interval <= size / 3)
-        interval = interval * 3 + 1;
+while (interval <= size / 3)
+interval = interval * 3 + 1;
 
-    while (interval > 0)
-    {
-        for (i = interval; i < size; i++)
-        {
-            tmp = array[i];
-            j = i;
-            while (j >= interval && array[j - interval] > tmp)
-            {
-                array[j] = array[j - interval];
-                j -= interval;
-            }
-            array[j] = tmp;
-        }
-        print_array(array, size);
-        interval = (interval - 1) / 3;
-    }
+while (interval > 0)
+{
+for (i = interval; i < size; i++)
+{
+tmp = array[i];
+j = i;
+while (j >= interval && array[j - interval] > tmp)
+{
+array[j] = array[j - interval];
+j -= interval;
+}
+array[j] = tmp;
+}
+print_array(array, size);
+interval = (interval - 1) / 3;
+}
 }
